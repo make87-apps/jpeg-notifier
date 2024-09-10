86d2f499f998f5921f7d7c1464bf13378774928f
nisseknudsen <nisse.knudsen@gmail.com>
2024-09-10 14:36:24 -0700

    Takes an incoming JPEG and sends it as ntfy.sh notification to `make87` topic. Does have a 30 sec cooldown.

--- app/main.py
+++ app/main.py
@@ -1,18 +1,48 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from io import BytesIO
 
-from make87_messages.text.PlainText_pb2 import PlainText
-from make87 import get_topic, topic_names, SubscriberTopic
+import requests
+from make87 import get_topic, topic_names
+from make87_messages.image.ImageJPEG_pb2 import ImageJPEG
+
+cooldown = 30  # seconds
+last_sent_time = datetime.min
+
+
+def send_notification_with_image(image_data: bytes):
+    global last_sent_time
+    current_time = datetime.now()
+
+    if current_time < last_sent_time + timedelta(seconds=cooldown):
+        print("Cooldown in effect, skipping notification.")
+        return
+
+    buffer = BytesIO(image_data)
+
+    # Send the notification
+    response = requests.post(
+        "https://ntfy.sh/make87",
+        data=image_data,  # "image_data".encode("utf-8"),  # Message body
+        headers={
+            "Title": "New Image Notification",
+            "Tags": "skull",
+            "Filename": "suspect.jpg",
+        },
+    )
+
+    if response.ok:
+        print("Notification sent successfully.")
+        last_sent_time = current_time
+    else:
+        print(f"Failed to send notification: {response.reason}")
 
 
 def main():
-    topic = get_topic(name=topic_names.INCOMING_MESSAGE)
+    topic = get_topic(name=topic_names.IMAGE_DATA)
 
-    def callback(message: PlainText):
-        received_dt = datetime.now()
-        publish_dt = message.timestamp.ToDatetime()
-        print(
-            f"Received message '{message.body}'. Sent at {publish_dt}. Received at {received_dt}. Took {(received_dt - publish_dt).total_seconds()} seconds."
-        )
+    def callback(message: ImageJPEG):
+        image_data = message.data
+        send_notification_with_image(image_data)
 
     topic.subscribe(callback)
 
